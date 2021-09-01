be6d5c9e482b7e52a35d097229c12ea938aeb036
Ocph23 <ocph23@gmail.com>
2021-09-02 05:23:36 +0900

    fix2

--- WepApp/Services/IEmailSerice.cs
+++ WepApp/Services/IEmailSerice.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
-using MimeKit;
+//using MimeKit;
+
 
 namespace WebApp.Services
 {
@@ -25,42 +28,54 @@ namespace WebApp.Services
             _mailSettings = mailSettings.Value;
         }
 
-        public async Task<bool> SendEmail(MailRequest mailRequest)
+        public Task<bool> SendEmail(MailRequest mailRequest)
         {
           try
           {
-            var email = new MimeMessage();
-            email.Sender = new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail);
-            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            var email = new System.Net.Mail.MailMessage();
+            //email.Sender = new System.Net.Mail.MailAddress(_mailSettings.DisplayName, _mailSettings.Mail);
+            email.To.Add(mailRequest.ToEmail);
             email.Subject = mailRequest.Subject;
-            var builder = new BodyBuilder();
-            if (mailRequest.Attachments != null)
-            {
-                byte[] fileBytes;
-                foreach (var file in mailRequest.Attachments)
-                {
-                    if (file.Length > 0)
-                    {
-                        using (var ms = new MemoryStream())
-                        {
-                            file.CopyTo(ms);
-                            fileBytes = ms.ToArray();
-                        }
-                        builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
-                    }
-                }
-            }
-            builder.HtmlBody = mailRequest.Body;
-            email.Sender.Name=_mailSettings.DisplayName;
-            email.Body = builder.ToMessageBody();
-            using var smtp = new SmtpClient();
-            smtp.CheckCertificateRevocation = false;
-            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
-            //await smtp.ConnectAsync(_mailSettings.SmtpServer, _mailSettings.Port, false);
-            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
-            await smtp.SendAsync(email);
-            smtp.Disconnect(true);
-            return true;
+                //var builder = new BodyBuilder();
+                //if (mailRequest.Attachments != null)
+                //{
+                //    byte[] fileBytes;
+                //    foreach (var file in mailRequest.Attachments)
+                //    {
+                //        if (file.Length > 0)
+                //        {
+                //            using (var ms = new MemoryStream())
+                //            {
+                //                file.CopyTo(ms);
+                //                fileBytes = ms.ToArray();
+                //            }
+                //            builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
+                //        }
+                //    }
+                //}
+                //builder.HtmlBody = mailRequest.Body;
+                //email.Sender.Name=_mailSettings.DisplayName;
+                //email.Body = builder.ToMessageBody();
+                email.From = new MailAddress(_mailSettings.Mail, "Pertamina KIM");
+                email.Body = mailRequest.Body;
+                 using var smtp = new System.Net.Mail.SmtpClient( _mailSettings.Host, _mailSettings.Port);
+
+                smtp.EnableSsl = true;
+
+                smtp.UseDefaultCredentials = false;
+
+                smtp.Credentials = new NetworkCredential(_mailSettings.Mail, _mailSettings.Password);
+                // =========================================================
+
+                smtp.Send(email);
+
+
+
+                //smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                //smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+                //await smtp.SendAsync(email);
+                //smtp.Disconnect(true);
+                return Task.FromResult(true);
           }
           catch (System.Exception ex)
           {

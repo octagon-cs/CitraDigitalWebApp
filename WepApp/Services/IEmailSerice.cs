06361c1e78a64b3fc621483acbde84a8d0f43125
Ocph23 <ocph23@gmail.com>
2021-09-02 14:19:13 +0900

    test2

--- WepApp/Services/IEmailSerice.cs
+++ WepApp/Services/IEmailSerice.cs
@@ -55,7 +55,7 @@ namespace WebApp.Services
                 builder.HtmlBody = mailRequest.Body;
                 email.Body = builder.ToMessageBody();
                 using var smtp = new SmtpClient();
-                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.SslOnConnect);
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
                 smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
                 await smtp.SendAsync(email);
                 smtp.Disconnect(true);

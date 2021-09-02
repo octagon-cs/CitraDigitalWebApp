db4b1531c7950e5da2ec39174dd40967019c50a0
Ocph23 <ocph23@gmail.com>
2021-09-02 20:21:02 +0900

    email

--- WepApp/Services/IEmailSerice.cs
+++ WepApp/Services/IEmailSerice.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
-using MailKit.Net.Smtp;
-using MailKit.Security;
+//using MailKit.Net.Smtp;
+//using MailKit.Security;
+//using MimeKit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using MimeKit;
-
 
 namespace WebApp.Services
 {
@@ -27,14 +28,15 @@ namespace WebApp.Services
             _mailSettings = mailSettings.Value;
         }
 
-        public async Task<bool> SendEmail(MailRequest mailRequest)
+        public Task<bool> SendEmail(MailRequest mailRequest)
         {
           try
           {
-                var email = new MimeMessage();
-                email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
-                email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
-                email.Subject = mailRequest.Subject;
+                string to = mailRequest.ToEmail;
+                string from = _mailSettings.Mail;
+                MailMessage message = new MailMessage(from, to);
+                message.Subject = "Using the new SMTP client.";
+
                 var builder = new BodyBuilder();
                 if (mailRequest.Attachments != null)
                 {
@@ -52,14 +54,54 @@ namespace WebApp.Services
                         }
                     }
                 }
-                builder.HtmlBody = mailRequest.Body;
-                email.Body = builder.ToMessageBody();
-                using var smtp = new SmtpClient();
-                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
-                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
-                await smtp.SendAsync(email);
-                smtp.Disconnect(true);
-                return true;
+                builder.HtmlBody =  mailRequest.Body;
+                message.Body = mailRequest.Body;
+                message.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient(_mailSettings.Host,_mailSettings.Port);
+                smtp.Credentials = new NetworkCredential(_mailSettings.Mail, _mailSettings.Password);
+                smtp.EnableSsl = true; 
+                smtp.UseDefaultCredentials = false;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                try
+                {
+                    smtp.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception caught in CreateTestMessage2(): {0}",
+                        ex.ToString());
+                }
+
+                //var email = new MimeMessage();
+                //email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+                //email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+                //email.Subject = mailRequest.Subject;
+                //var builder = new BodyBuilder();
+                //if (mailRequest.Attachments != null)
+                //{
+                //    byte[] fileBytes;
+                //    foreach (var file in mailRequest.Attachments)
+                //    {
+                //        if (file.Length > 0)
+                //        {
+                //            using (var ms = new MemoryStream())
+                //            {
+                //                file.CopyTo(ms);
+                //                fileBytes = ms.ToArray();
+                //            }
+                //            builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
+                //        }
+                //    }
+                //}
+                //builder.HtmlBody = "Ini pesan senderhana !";// mailRequest.Body;
+                //email.Body =  builder.ToMessageBody();
+                //using var smtp = new SmtpClient();
+                //smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                //smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+                //await smtp.SendAsync(email);
+                //smtp.Disconnect(true);
+                return Task.FromResult(true);
           }
           catch (System.Exception ex)
           {

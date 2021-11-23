93151df44f588fe3a0b4e934b270c90ce9f39af9
Ajenkris Kungkung <kristt26@gmail.com>
2021-11-23 13:55:05 +0900

    ok

--- WepApp/Services/IEmailSerice.cs
+++ WepApp/Services/IEmailSerice.cs
@@ -63,7 +63,7 @@ namespace WebApp.Services
         public string ToEmail { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
-        public List<IFormFile> Attachments { get; set; }
+        //public List<IFormFile> Attachments { get; set; }
     }
 
 
